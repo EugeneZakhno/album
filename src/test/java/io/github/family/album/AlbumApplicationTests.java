8b39a692a9f50584da76f7168fbd2356f36af428
EugeneZakhno <zohan1234567@gmail.com>
2020-09-24 22:09:16 +0300

    init

---
+++ src/test/java/io/github/family/album/AlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.family.album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
